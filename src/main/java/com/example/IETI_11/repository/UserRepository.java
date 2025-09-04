2ac57bbd502036e3b05f10e45d412f1833bafb09
cesar <cesar.amaya.gomes@gmail.com>
2025-09-04 10:58:54 -0500

    no message

--- src/main/java/com/example/IETI_11/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.IETI_11.repository;
-
-import com.example.IETI_11.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
